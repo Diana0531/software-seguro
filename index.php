0c5c7a1400ebb8e36da55811074aefa6a0c971b6
Diana0531 <reveylleromero@gmail.com>
2026-04-19 12:50:10 -0500

    correccion vulnerabilidad include

--- index.php
+++ index.php
@@ -2,52 +2,30 @@
     ob_start();
     session_start();
     // session_destroy();
-
     require_once "models/DataBase.php";
-
-    $controllerRequest = isset($_REQUEST['c']) ? $_REQUEST['c'] : "Landing";
-    $action = isset($_REQUEST['a']) ? $_REQUEST['a'] : 'main';
-
-    $allowedControllers = [
-        "Landing" => "Landing",
-        "Login" => "Login",
-        "User" => "User",
-        "Category" => "Category",
-        "Product" => "Product",
-        "Sale" => "Sale",
-        "Purchase" => "Purchase",
-        "Dashboard" => "Dashboard"
-    ];
-
-    if (!array_key_exists($controllerRequest, $allowedControllers)) {
-        $controllerRequest = "Landing";
-    }
-
-    $controllerName = $allowedControllers[$controllerRequest];
-    $route_controller = "controllers/" . $controllerName . ".php";
-
+    $controller = isset($_REQUEST['c']) ? $_REQUEST['c'] : "Landing";
+    $route_controller = "controllers/" . $controller . ".php";
     if (file_exists($route_controller)) {
-        $view = $controllerName;
+        $view = $controller;
         require_once $route_controller;
-        $controller = new $controllerName;
-
+        $controller = new $controller;
+        $action = isset($_REQUEST['a']) ? $_REQUEST['a'] : 'main';
         if ($view === 'Landing' || $view === 'Login') {
             require_once "views/company/header.view.php";
-            call_user_func([$controller, $action]);
+            call_user_func(array($controller, $action));
             require_once "views/company/footer.view.php";
         } elseif (!empty($_SESSION['session'])) {
             require_once "models/User.php";
             $profile = unserialize($_SESSION['profile']);
             $session = $_SESSION['session'];
-            require_once "views/roles/" . $session . "/header.view.php";
-            call_user_func([$controller, $action]);
-            require_once "views/roles/" . $session . "/footer.view.php";
+            require_once "views/roles/".$session."/header.view.php";
+            call_user_func(array($controller, $action));
+            require_once "views/roles/".$session."/footer.view.php";
         } else {
             header("Location:?");
         }
     } else {
         header("Location:?");
     }
-
     ob_end_flush();
 ?>
