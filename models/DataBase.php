c3f195f1d443e5e11115fc20c6c64d11bd346296
Diana0531 <reveylleromero@gmail.com>
2026-04-20 19:55:52 -0500

    Correccion seguridad contraseña BD

--- models/DataBase.php
+++ models/DataBase.php
@@ -6,7 +6,7 @@
             $port = "3307";
             $database = "db_inventory";
             $username = "root";
-            $password = "Diana0531@";
+            $password = getenv('MYSQL_SECURE_PASSWORD') ?: "Diana0531@";
 			$pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password);
 			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			return $pdo;
