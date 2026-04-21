02faf7f938799c97ed6f63b8a8624eed263f4f98
Diana0531 <reveylleromero@gmail.com>
2026-04-20 19:47:18 -0500

    Correccion seguridad base de datos con contraseña

--- models/DataBase.php
+++ models/DataBase.php
@@ -6,7 +6,7 @@
             $port = "3307";
             $database = "db_inventory";
             $username = "root";
-            $password = "";
+            $password = "Diana0531@";
 			$pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password);
 			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			return $pdo;
